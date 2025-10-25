f074400f16d7339965e175b7512a9d8c3f1f436e
faiq fauzi <faiqfauzi101@gmail.com>
2025-10-25 13:31:04 +0700

    commit 2

---
+++ app/Models/ProductTransaction.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductTransaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'booking_trx_id',
+        'city',
+        'post_code',
+        'address',
+        'quantity',
+        'sub_total_amount',
+        'grand_total_amount',
+        'is_paid',
+        'product_id',
+        'proof'
+    ];
+
+    public static function generateUniqueTrxId()
+    {
+        $prefix = 'Mads';
+        do {
+            $randomString = $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx_id', $randomString)->exist());
+        return $randomString;
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+}
