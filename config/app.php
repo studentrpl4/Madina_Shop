410138c81ab44c01602997b310322c5159ddb1f5
faiq fauzi <faiqfauzi101@gmail.com>
2025-11-15 22:21:58 +0700

    commit 3 (Category Filament)

--- config/app.php
+++ config/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
+
 return [
 
     /*
@@ -167,6 +168,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
